8220ed8df6a2a397e065b75ec5a67ce325729e68
k5-mina <any.minak.0122@gmail.com>
2026-07-08 00:19:05 +0900

    Angularアプリケーションプログラミング 3.3.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: '<div>{{msg}}</div>',
 })
 export class App {
-  name = 'Angular';
+  msg: string = `<script>window.alert("ようこそ！"):</script>
+          <div style="font-size:20px;">
+            <p>WINGSプロジェクト</p>
+          </div>
+          <a href="http://www.wings.msn.to/">Web</a>
+          <button>同意する</button>
+          <input type="button" onclick="alert('OK')" value="クリック" />`;
 }
