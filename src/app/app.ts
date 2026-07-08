eca193f1fc1e1b2fda4ea6ccdbbc9519a8511a09
k5-mina <any.minak.0122@gmail.com>
2026-07-08 23:22:36 +0900

    3.3.3 リスト3-10

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  template: '<div [innerHTML]="safeMsg"></div>',
+  template: `
+    <iframe [src]="url"></iframe>
+    `,
 })
 export class App {
-  safeMsg: SafeHtml;
-  msg: string = `<script>window.alert("ようこそ！"):</script>
-          <div style="font-size:20px;">
-            <p>WINGSプロジェクト</p>
-          </div>
-          <a href="http://www.wings.msn.to/">Web</a>
-          <button>同意する</button>
-          <input type="button" onclick="alert('OK')" value="クリック" />`;
-
-  constructor(private sanitizer: DomSanitizer) {
-    // msgプロパティの内容に信頼済みマークを付与
-    this.safeMsg = sanitizer.bypassSecurityTrustHtml(this.msg);
-  }
+  url = 'http://www.wings.msn.to/';
 }
