ca6ab8a9ade3207c8951b03dc8531d94d44ee73e
k5-mina <any.minak.0122@gmail.com>
2026-07-08 23:28:05 +0900

    3.3.3 リスト3-11

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: `
-    <iframe [src]="url"></iframe>
+    <iframe [src]="safeUrl"></iframe>
     `,
 })
 export class App {
+  safeUrl: SafeResourceUrl;
   url = 'http://www.wings.msn.to/';
+
+  // リソースが信頼済みであることをマークする
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeUrl = sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 }
