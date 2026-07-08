4bfe220c3bff3d08f293802c1ab3322a587423b6
k5-mina <any.minak.0122@gmail.com>
2026-07-08 23:43:02 +0900

    3.4.1 リスト3-13

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<table border="1">
-    <tr><td [rowspan]="len">結合</td><td>1</td><tr>
-    <tr><td>2</td><tr>
-    <tr><td>3</td><tr>
+    <tr><td [attr.rowspan]="len">結合</td><td>1</td></tr>
+    <tr><td>2</td></tr>
+    <tr><td>3</td></tr>
     </table>`,
 })
 export class App {
